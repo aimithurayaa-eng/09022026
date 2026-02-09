2222b35fe05dcbdf23890ec94a990739a7a8e617
aimithurayaa-eng <aimithurayaa@gmail.com>
2026-02-09 09:37:52 +0800

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,22 +1,23 @@
-
 import { GoogleGenAI } from "@google/genai";
 import { RAW_CSV_DATA, SYSTEM_INSTRUCTION } from "../constants";
 
 export const callGemini = async (prompt: string) => {
   try {
-    const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
-    const fullSystemInstruction = `${SYSTEM_INSTRUCTION}\n\nData CSV:\n${RAW_CSV_DATA}`;
-    
+    const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY });
+
+    const fullSystemInstruction =
+      `${SYSTEM_INSTRUCTION}\n\nData CSV:\n${RAW_CSV_DATA}`;
+
     const response = await ai.models.generateContent({
-      model: 'gemini-3-pro-preview',
+      model: "gemini-2.5-flash", // lihat nota model di bawah
       contents: prompt,
-      config: { 
-        systemInstruction: fullSystemInstruction, 
-        temperature: 0.1 
-      }
+      config: {
+        systemInstruction: fullSystemInstruction,
+        temperature: 0.1,
+      },
     });
-    
-    return response.text || "Maaf, sistem JOMRUMAHBOT tidak dapat memproses data buat masa ini.";
+
+    return response.text || "Maaf, sistem tidak dapat memproses data buat masa ini.";
   } catch (error) {
     console.error("Gemini API Error:", error);
     throw error;
